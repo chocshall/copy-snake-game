a189c8425d50e8e85736367c2e51cfd6ca26f49f
chocshall <chocshall@gmail.com>
2026-01-13 10:46:22 +0200

    body parts of main body move with body

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -1,44 +1,67 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
 
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 1f;
     public char currentDirection = 'S';
     public Vector2 defaultPosition;
-    public float historyBefore;
-    public float history;
-   
-    private Vector2 movementDirection = Vector2.down;
+    public Vector2 historyBefore;
+    public Vector2 history;
 
+
+    public Vector2 movementDirection = Vector2.down;
+    public Vector2 historyDirection;
+    public Vector2 defaultDirection;
+
+    public Vector2 historyMovementBefore;
+    public Vector2 historyMovement;
+    
+
+    void Start()
+    {
+      
+            InvokeRepeating(nameof(MovementBy35), 0f, 1.0f);
+    }
     void Update()
     {
         // Always move in the current direction
+        historyDirection = movementDirection;
+
         
-        if(transform.position.y < -3.6f || transform.position.y > 4.8f || transform.position.x < -2f || transform.position.x > 2f)
+        if (transform.position.y < -3.6f || transform.position.y > 4.8f || transform.position.x < -2f || transform.position.x > 2f)
         {
             ResetToDefault();
-            
+
         }
-        historyBefore = transform.position.y;
-        transform.Translate(movementDirection * moveSpeed * Time.deltaTime);
-        history = transform.position.y;
+        historyBefore = transform.position;
+
+
+        //transform.Translate(movementDirection * moveSpeed * Time.deltaTime);
+
+
+
+        history = transform.position;
 
 
     }
 
-    
+
     public void SetDirectionUp()
     {
+
         if (movementDirection != Vector2.down)
         {
             movementDirection = Vector2.up;
+            moveSpeed = 3f;
         }
         else
         {
             movementDirection = Vector2.down;
         }
-        
+
     }
 
     public void SetDirectionDown()
@@ -46,25 +69,27 @@ public class Movement : MonoBehaviour
         if (movementDirection != Vector2.up)
         {
             movementDirection = Vector2.down;
+            moveSpeed = 1f;
         }
         else
         {
             movementDirection = Vector2.up;
         }
-        
+
     }
 
     public void SetDirectionLeft()
     {
-        if(movementDirection != Vector2.right)
+        if (movementDirection != Vector2.right)
         {
             movementDirection = Vector2.left;
+            moveSpeed = 2f;
         }
         else
         {
             movementDirection = Vector2.right;
         }
-        
+
     }
 
     public void SetDirectionRight()
@@ -72,18 +97,22 @@ public class Movement : MonoBehaviour
         if (movementDirection != Vector2.left)
         {
             movementDirection = Vector2.right;
+            moveSpeed = 4f;
         }
         else
         {
             movementDirection = Vector2.left;
         }
-        
+
     }
 
     private void Awake()
     {
         defaultPosition = transform.position;
-       
+        defaultDirection = movementDirection;
+
+
+
     }
 
     private void ResetToDefault()
@@ -91,4 +120,70 @@ public class Movement : MonoBehaviour
         transform.position = defaultPosition;
         moveSpeed = 0f;
     }
+
+    private void MovementBy35()
+    {
+        Vector2 movementBefore = transform.position;
+
+        // Store all children
+        List<Transform> children = new List<Transform>();
+        List<Vector2> childPositions = new List<Vector2>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children.Add(transform.GetChild(i));
+            childPositions.Add(transform.GetChild(i).position);
+        }
+        foreach (Transform child in children)
+        {
+            child.SetParent(null);
+        }
+        if (moveSpeed == 1f)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.35f);
+        }
+        
+        if (moveSpeed == 2f)
+        {
+            transform.position = new Vector2(transform.position.x - 0.35f, transform.position.y);
+        }
+
+        if (moveSpeed == 3f)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + 0.35f);
+        }
+
+        if (moveSpeed == 4f)
+        {
+           transform.position = new Vector2(transform.position.x + 0.35f, transform.position.y);
+        }
+
+
+
+        Vector2 nextPosition = movementBefore;
+
+        for (int i = 0; i < children.Count; i++)
+        {
+            children[i].position = childPositions[i];
+            children[i].SetParent(transform);
+        }
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            Vector2 childOldPosition = child.transform.position;
+
+            // Move this child to the previous position in the chain
+            //Vector2 localPos = transform.InverseTransformPoint(nextPosition);
+            child.transform.position = nextPosition;
+            //child.localPosition = localPos;
+
+            // Next child will go to this child's old position
+            nextPosition = childOldPosition;
+        }
+
+
+
+    }
+
+
+
 }
--- Assets/scripts/movementOfBodyParts.cs
+++ Assets/scripts/movementOfBodyParts.cs
@@ -3,31 +3,75 @@ using UnityEngine;
 public class MovementOfBodyParts : MonoBehaviour
 {
     private GameObject player;
-    private float playerPosition;
+    private (float, float) playerPosition;
+    private Vector2 startDirection;
+    private Movement movementScript;
+
+    private Vector2 position;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindWithTag("Player");
-        playerPosition = player.GetComponent<Movement>().historyBefore;
+        movementScript = player.GetComponent<Movement>();
+        
+        startDirection = player.GetComponent<Movement>().defaultDirection;
+        position = transform.position;
+
+        InvokeRepeating(nameof(ApplyPositionChange), 0f, 1.0f);
+
     }
     
     // Update is called once per frame
     void Update()
     {
+        //transform.position = movementScript.historyMovementBefore;
+        
+        //float distance = Vector2.Distance(movementScript.historyBefore, movementScript.history);
 
-        if(player.GetComponent<Movement>().historyBefore != player.GetComponent<Movement>().history)
+        //transform.Translate(movementScript.historyDirection * distance);
+
+        if (player.GetComponent<Movement>().moveSpeed == 0f)
         {
-            float distance = player.GetComponent<Movement>().history - player.GetComponent<Movement>().historyBefore;
-            transform.position = new Vector2(transform.position.x, transform.position.y + distance);
-            
-            //Vector2 playerDistance = player.GetComponent<Movement>().history - player.GetComponent<Movement>().historyBefore;
-            playerPosition = player.GetComponent<Movement>().history;
-
+            transform.position = new Vector2(0, 0.35f);
         }
-        //if (player.defaultPosition == (0,0,0))
-        //{
-        //    transform.position = player.defaultPosition;
-        //}
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("body"))
+        {
+            collision.gameObject.SendMessage("ApplyPositionChange", 0.35f);
+        }
+    }
+
+    public void ApplyPositionChange(float  position)
+    {
+        if (movementScript.moveSpeed == 1f)
+        {
+            //transform.position = new Vector2(transform.position.x, transform.position.y - 0.35f);
+            transform.localPosition = new Vector2(0, 0 + 0.35f);
+        }
+
+        if (movementScript.moveSpeed == 2f)
+        {
+            //transform.position = new Vector2(transform.position.x - 0.35f, transform.position.y);
+            transform.localPosition = new Vector2(0 + 0.35f,0);
+        }
+
+        if (movementScript.moveSpeed == 3f)
+        {
+            //transform.position = new Vector2(transform.position.x, transform.position.y + 0.35f);
+            transform.localPosition = new Vector2(0, 0 - 0.35f);
+        }
+
+        if (movementScript.moveSpeed == 4f)
+        {
+            //transform.position = new Vector2(transform.position.x + 0.35f, transform.position.y);
+            transform.localPosition = new Vector2(0 - 0.35f, 0);
+        }
+        
     }
 }
